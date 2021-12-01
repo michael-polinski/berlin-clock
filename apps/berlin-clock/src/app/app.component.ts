406496eb0dbb83e1549cc75da70619c945abd987
koenigkatze <michaelpolinski@t-online.de>
2021-12-01 10:11:55 +0100

    initial commit

---
+++ apps/berlin-clock/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'berlin-clock-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'berlin-clock';
+}
